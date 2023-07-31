f0604242aabfed2d5fbb7942df42cea9099f0c3d
Quentin <nelsonsims@hotmail.fr>
2023-07-31 17:25:37 +0200

    chore: Add method to reward the player

--- src/main/kotlin/com/github/rushyverse/rtf/client/ClientRTF.kt
+++ src/main/kotlin/com/github/rushyverse/rtf/client/ClientRTF.kt
@@ -1,6 +1,7 @@
 package com.github.rushyverse.rtf.client
 
 import com.github.rushyverse.api.player.Client
+import com.github.rushyverse.rtf.config.Reward
 import kotlinx.coroutines.CoroutineScope
 import java.util.*
 
@@ -11,4 +12,20 @@ class ClientRTF(
     scope: CoroutineScope
 ) : Client(pluginId, uuid, scope) {
 
+    /**
+     * Gives instantly a reward to the client.
+     * For each positive value in the [Reward], the client will receive
+     * a notification message and sound.
+     * @param reward The reward to apply on the client.
+     */
+    fun reward(reward: Reward) {
+        if (reward.coins > 0) {
+            // TODO: inc coins value
+            send("§E+${reward.coins} coins")
+        }
+        if (reward.xp > 0) {
+            requirePlayer().giveExp(reward.xp)
+            send("§D+${reward.xp} xp")
+        }
+    }
 }
